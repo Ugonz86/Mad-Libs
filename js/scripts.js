fa94f00a1c83438b7e861896428c352910baf94c
Sharon Lee and Uriel Gonzalez <uriel@>
2019-10-28 13:51:37 -0700

    Add refactored code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,35 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var excerciseInput= $("input#excercise").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+// OLD CODE
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     var person1Input = $("input#person1").val();
+//     var person2Input = $("input#person2").val();
+//     var excerciseInput= $("input#excercise").val();
+//     var exclamationInput = $("input#exclamation").val();
+//     var verbInput = $("input#verb").val();
+//     var nounInput = $("input#noun").val();
+//
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".excercise").text(excerciseInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+//
+//     $("#story").show();
+//
+//     event.preventDefault();
+//   });
+// });
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".excercise").text(excerciseInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+// NEW, REFACTORED CODE
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
